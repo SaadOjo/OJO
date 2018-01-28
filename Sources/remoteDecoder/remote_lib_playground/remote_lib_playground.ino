9630169efd67cd5c17e2d46c8a6a98b9acc7a9e7
DESKTOP-T3N8756\Solemon <saadsaif0333@gmail.com>
2018-01-28 15:53:11 +0300

    something

--- Sources/remoteDecoder/remote_lib_playground/remote_lib_playground.ino
+++ Sources/remoteDecoder/remote_lib_playground/remote_lib_playground.ino
@@ -3,11 +3,14 @@
 remoteDecoder myDecoder(2);
 
 int decoded;
+bool leaveState = false;
 
 void setup()
 {
   Serial.begin(115200);
   myDecoder.init();
+  pinMode(3,OUTPUT);
+  digitalWrite(3,LOW);
 }
 
 void loop()
@@ -15,6 +18,8 @@ void loop()
  bool test = myDecoder.detectPulse();
   if(test)
   {
+    leaveState = !leaveState;
+    digitalWrite(3,leaveState);
     Serial.println("Pulse Detected!");
     decoded = myDecoder.decode();
     Serial.println(String("Input Recieved: ") + decoded);
