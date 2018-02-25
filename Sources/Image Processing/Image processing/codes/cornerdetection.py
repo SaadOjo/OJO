c6abc381842a122b0ef362d42d6611f96d34e6fd
bulukapi <ulukapi.bulut@metu.edu.tr>
2018-02-25 23:49:22 +0300

    modified for camera,need improvement

--- Sources/Image Processing/Image processing/codes/cornerdetection.py
+++ Sources/Image Processing/Image processing/codes/cornerdetection.py
@@ -1,20 +1,36 @@
 import numpy as np
 import cv2
+import sys
 
-#load the image and convert it to gray and then to float32
-image=cv2.imread('example_image.jpg')
-gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-gray=np.float32(gray)
+cap = cv2.VideoCapture(0)
+#set the width,height, and exposure time
+cap.set(3,1080)
+cap.set(4,1024)
+cap.set(15, 0.1)
 
+def corner(filename) : 
+ im= filename
+
+ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #detect corners with the goodFeaturesToTrack function
 #parameters are as follows (image, max corners to detect, quality(between 0-1), and minimum euclidean distance between corners)
-corners = cv2.goodFeaturesToTrack(gray, 25, 0.01, 20)
-corners = np.int0(corners)
+ corners = cv2.goodFeaturesToTrack(gray,25,0.01,20)
+ corners = np.int0(corners)
 
-for corner in corners:
-    x,y = corner.ravel()
-    cv2.circle(image,(x,y),3,255,-1)
-    
-cv2.imshow('Corner',image)
-cv2.waitKey(0)
+ for i in corners:
+    x,y = i.ravel()
+    cv2.circle(img,(x,y),3,(0,0,255),-1)
+ return im
+
+while True:
+    ret, img = cap.read()
+    cv2.imshow("input",corner(img))
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+
+
+cv2.destroyAllWindows() 
+cv2.VideoCapture(0).release()
+
 
