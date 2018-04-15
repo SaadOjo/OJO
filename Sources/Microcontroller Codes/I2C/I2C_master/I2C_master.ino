0b988a9835029cf885b5143933b9816666f226da
bulukapi <ulukapi.bulut@metu.edu.tr>
2018-04-15 16:11:29 +0300

    I2C for arduino communication

---
+++ Sources/Microcontroller Codes/I2C/I2C_master/I2C_master.ino
@@ -0,0 +1,30 @@
+//Arduino I2C code for the master
+#include <Wire.h>
+#define slaveAddress 0x80   
+
+void setup() {
+  
+    Wire.begin();
+    Serial.begin(9600);
+    
+}
+
+byte b = 0; 
+
+void loop() {
+
+    b = random(0,255);
+    Wire.beginTransmission(slaveAddress);   
+    Wire.write(b);                
+    Wire.endTransmission();   
+    delay(100);
+    
+    Wire.requestFrom(slaveAddress, 1);
+
+    int howMany = Wire.available();
+    for(int i = 0; i<howMany; i++) {
+    
+          b = Wire.read();
+          Serial.print(b);
+    }
+}
---
+++ Sources/Microcontroller Codes/I2C/I2C_slave/I2C_slave.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#define slaveAddress 0x80
+byte b = 0;
+void setup()
+{
+  Wire.begin(slaveAddress);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  delay(100);
+}
+
+void requestEvent() 
+{
+  Wire.write(b);
+}
+
+void receiveEvent(int howMany)
+{
+  if(Wire.available() != 0)
+  {
+   //  for(int i = 0; i<howMany; i++) { 
+        b = Wire.read();
+        Serial.print(b);
+
+        switch (b) {
+
+          case 0b00000001:
+          //go forward
+          break;
+          case 0b00000010:
+          //go left
+          break;
+          case 0b00000011:
+          //go right
+          break;
+          case 0b00010000:
+          //leave command
+          break;
+   
+          
+    }  
+//   } 
+  }
+}
