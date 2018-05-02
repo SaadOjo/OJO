805cd39fc9b0801efc148e85bb9cfa50993a182e
DESKTOP-T3N8756\Solemon <saadsaif0333@gmail.com>
2018-05-03 00:40:14 +0300

    sth tried does not work

--- Sources/Microcontroller Codes/sensor_library/sensor_lib_playground/infraredDirection.cpp
+++ Sources/Microcontroller Codes/sensor_library/sensor_lib_playground/infraredDirection.cpp
@@ -44,25 +44,31 @@ float infraredDirection::getDirection()
 {
   _direction = (_rightVoltage - _leftVoltage)*20; //don't need saturation (no scaling required)
 
-  if(_direction<-5)
+  if(_direction<0)
   {
-    _direction = -70;
-  }
-  else if(_direction>5)
-  {
-    _direction = 70;
-  }
-  else
-  { 
-    if(_direction > 0)
+    if(_direction<-5)
     {
-      _direction = 1;
+      _direction = -70;
     }
     else
     {
-      _direction = -1;
+      _direction = -2;
+    }
+    
+  }
+  else
+  {
+    if(_direction > 5)
+    {
+    _direction = 70;
+    }
+    else
+    {
+      _direction = 2;
     }
   }
+
+
   
   
 
